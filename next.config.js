4ada167826b0a635f9239e40678ee8be99168f07
CrisanySifi97 <Franciscosifi@outlook.com>
2026-04-09 18:16:53 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'media.api-sports.io',   // logos das equipas
+      'media-4.api-sports.io',
+    ],
+  },
+};
+
+module.exports = nextConfig;
